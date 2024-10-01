1067f3627a21e8a817d90da98f10677c2bc72090
Procos12 <choskib@gmail.com>
2024-10-01 16:43:16 +0300

    fixed spacing issue in threadTools.js converted indentation from spaces to tabs in best-response.js

--- public/src/client/topic/best-response.js
+++ public/src/client/topic/best-response.js
@@ -1,98 +1,98 @@
 'use strict';
 
 define('forum/topic/delete-posts', [
-    'postSelect', 'alerts', 'api',
+	'postSelect', 'alerts', 'api',
 ], function (postSelect, alerts, api) {
-    const DeletePosts = {};
-    let modal;
-    let markBtn;
-    let purgeBtn;
-    let tid;
+	const DeletePosts = {};
+	let modal;
+	let markBtn;
+	let purgeBtn;
+	let tid;
 
-    DeletePosts.init = function () {
-        tid = ajaxify.data.tid;
+	DeletePosts.init = function () {
+		tid = ajaxify.data.tid;
 
-        $(window).off('action:ajaxify.end', onAjaxifyEnd).on('action:ajaxify.end', onAjaxifyEnd);
+		$(window).off('action:ajaxify.end', onAjaxifyEnd).on('action:ajaxify.end', onAjaxifyEnd);
 
-        if (modal) {
-            return;
-        }
-        //Below is how this file links to the tpl file and how it represents it
-        //modified file below to redirect to best-response.tpl
-        app.parseAndTranslate('modals/best-response', {}, function (html) {
-            modal = html;
+		if (modal) {
+			return;
+		}
+		//Below is how this file links to the tpl file and how it represents it
+		//modified file below to redirect to best-response.tpl
+		app.parseAndTranslate('modals/best-response', {}, function (html) {
+			modal = html;
 
-            $('body').append(modal);
+			$('body').append(modal);
 
-            //These are the buttons that are on the modal (other than close)
-            markBtn = modal.find('#markResponse_posts_confirm');
-            purgeBtn = modal.find('#purge_posts_confirm');
+			//These are the buttons that are on the modal (other than close)
+			markBtn = modal.find('#markResponse_posts_confirm');
+			purgeBtn = modal.find('#purge_posts_confirm');
 
-            modal.find('#markResponse_posts_cancel').on('click', closeModal);
+			modal.find('#markResponse_posts_cancel').on('click', closeModal);
 
-            postSelect.init(function () {
-                checkButtonEnable();
-                showPostsSelected();
-            });
-            showPostsSelected();
+			postSelect.init(function () {
+				checkButtonEnable();
+				showPostsSelected();
+			});
+			showPostsSelected();
 
-            //Linking the buttons to the functionality
-            markBtn.on('click', function () {
-                console.log('Clicked on mark');
-                markBestResponse(markBtn, pid => `/posts/${pid}/state`);
-            });
-            //ignore the purge button from now
-            purgeBtn.on('click', function () {
-                markBestResponse(purgeBtn, pid => `/posts/${pid}`);
-            });
-        });
-    };
+			//Linking the buttons to the functionality
+			markBtn.on('click', function () {
+				console.log('Clicked on mark');
+				markBestResponse(markBtn, pid => `/posts/${pid}/state`);
+			});
+			//ignore the purge button from now
+			purgeBtn.on('click', function () {
+				markBestResponse(purgeBtn, pid => `/posts/${pid}`);
+			});
+		});
+	};
 
-    function onAjaxifyEnd() {
-        if (ajaxify.data.template.name !== 'topic' || ajaxify.data.tid !== tid) {
-            closeModal();
-            $(window).off('action:ajaxify.end', onAjaxifyEnd);
-        }
-    }
+	function onAjaxifyEnd() {
+		if (ajaxify.data.template.name !== 'topic' || ajaxify.data.tid !== tid) {
+			closeModal();
+			$(window).off('action:ajaxify.end', onAjaxifyEnd);
+		}
+	}
 
-    //Edit this function to mark a post as the best response 
-    function markBestResponse(btn, route) {
-        btn.attr('disabled', true);
-        //-Change the function below to mark as a best response here in the UI
-        console.log('marked');
-        Promise.all(postSelect.pids.map(pid => api.del(route(pid), {})))
-            .then(closeModal)
-            .catch(alerts.error)
-            .finally(() => {
-                btn.removeAttr('disabled');
-            });
-    }
+	//Edit this function to mark a post as the best response 
+	function markBestResponse(btn, route) {
+		btn.attr('disabled', true);
+		//-Change the function below to mark as a best response here in the UI
+		console.log('marked');
+		Promise.all(postSelect.pids.map(pid => api.del(route(pid), {})))
+			.then(closeModal)
+			.catch(alerts.error)
+			.finally(() => {
+				btn.removeAttr('disabled');
+			});
+	}
 
-    function showPostsSelected() {
-        if (postSelect.pids.length) {
-            modal.find('#pids').translateHtml('[[topic:fork-pid-count, ' + postSelect.pids.length + ']]');
-        } else {
-            modal.find('#pids').translateHtml('[[topic:fork-no-pids]]');
-        }
-    }
-    //Made sure you can only mark one post at a time in the UI
-    function checkButtonEnable() {
-        if (postSelect.pids.length==1) {
-            markBtn.removeAttr('disabled');
-            purgeBtn.removeAttr('disabled');
-        } else {
-            markBtn.attr('disabled', true);
-            purgeBtn.attr('disabled', true);
-        }
-    }
+	function showPostsSelected() {
+		if (postSelect.pids.length) {
+			modal.find('#pids').translateHtml('[[topic:fork-pid-count, ' + postSelect.pids.length + ']]');
+		} else {
+			modal.find('#pids').translateHtml('[[topic:fork-no-pids]]');
+		}
+	}
+	//Made sure you can only mark one post at a time in the UI
+	function checkButtonEnable() {
+		if (postSelect.pids.length==1) {
+			markBtn.removeAttr('disabled');
+			purgeBtn.removeAttr('disabled');
+		} else {
+			markBtn.attr('disabled', true);
+			purgeBtn.attr('disabled', true);
+		}
+	}
 
-    function closeModal() {
-        if (modal) {
-            modal.remove();
-            modal = null;
-            postSelect.disable();
-        }
-    }
+	function closeModal() {
+		if (modal) {
+			modal.remove();
+			modal = null;
+			postSelect.disable();
+		}
+	}
 
-    return DeletePosts;
+	return DeletePosts;
 });
--- public/src/client/topic/threadTools.js
+++ public/src/client/topic/threadTools.js
@@ -111,7 +111,7 @@ define('forum/topic/threadTools', [
 		});
 
 		// Code for best response, attempt 1.
-		// code segment above references delete-posts.js, will make a file for 
+		// code segment above references delete-posts.js, will make a file for
 		// best response
 		// Code links successfully to topic-menu-list.tpl when using [component="topic/best-response"] below
 		topicContainer.on('click', '[component="topic/best-response"]', function () {
