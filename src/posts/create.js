f3745c70a7ab4197a03b7ee09b01bceb5c04ea94
Abdallah <aalmana@andrew.cmu.edu>
2024-11-11 14:12:04 +0300

    translator

--- src/posts/create.js
+++ src/posts/create.js
@@ -9,6 +9,7 @@ const topics = require('../topics');
 const categories = require('../categories');
 const groups = require('../groups');
 const privileges = require('../privileges');
+const translate = require('../translate');
 
 
 module.exports = function (Posts) {
@@ -19,6 +20,7 @@ module.exports = function (Posts) {
 		// eslint-disable-next-line prefer-const
 		const timestamp = data.timestamp || Date.now();
 		const isMain = data.isMain || false;
+		const [isEnglish, translatedContent] = await translate.translate(data)
 
 		if (!uid && parseInt(uid, 10) !== 0) {
 			throw new Error('[[error:invalid-uid]]');
@@ -35,6 +37,8 @@ module.exports = function (Posts) {
 			content: data.content,
 			timestamp: timestamp,
 			anonymous: data.isAnonymous,
+			translatedContent: translatedContent,
+			isEnglish: isEnglish,
 		};
 
 		if (data.toPid) {
