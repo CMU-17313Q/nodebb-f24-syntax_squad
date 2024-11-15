4277dc0eddc1932fa19076ff927f9cecf7203f78
Reem Konsowa <reemkonsowa@Reems-MacBook-Air.local>
2024-11-15 12:40:10 +0300

    commiting to trigger deployment

--- src/translate/index.js
+++ src/translate/index.js
@@ -3,7 +3,8 @@
 const translatorApi = module.exports;
 
 translatorApi.translate = async function (postData) {
-	const TRANSLATOR_API = 'https://nodebb-f24-translator.azurewebsites.net/';
+	// Edit the translator URL below
+	const TRANSLATOR_API = 'translator-syntax-squad.azurewebsites.net';
 	const response = await fetch(`${TRANSLATOR_API}/?content=${postData.content}`);
 	const data = await response.json();
 	return [data.is_english, data.translated_content];
